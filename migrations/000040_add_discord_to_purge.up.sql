e089f8a5a647343bf95c4089b6d5d12ce1374984
Greg Holmes <git@greg.holmes.name>
2022-12-16 12:39:49 +0000

    Add discord notifications for deletes

---
+++ migrations/000040_add_discord_to_purge.up.sql
@@ -0,0 +1,31 @@
+CREATE OR REPLACE FUNCTION "internal"."announce_game_delete"() RETURNS TRIGGER
+    LANGUAGE "plpgsql"
+    SECURITY DEFINER AS
+$$
+BEGIN
+    EXECUTE internal.announce(FORMAT('💀 Deleted game: `%s`', OLD.code));
+    RETURN NULL;
+END
+$$;
+ALTER FUNCTION "internal"."announce_game_delete"() OWNER TO "postgres";
+CREATE OR REPLACE TRIGGER "announce_game_delete"
+    AFTER DELETE
+    ON "public"."games"
+    FOR EACH ROW
+EXECUTE FUNCTION "internal"."announce_game_delete"();
+CREATE OR REPLACE FUNCTION "internal"."announce_user_delete"() RETURNS TRIGGER
+    LANGUAGE "plpgsql"
+    SECURITY DEFINER AS
+$$
+BEGIN
+    EXECUTE internal.announce(FORMAT('💀 Deleted user: `%s`', OLD.id));
+    RETURN NULL;
+END
+$$;
+ALTER FUNCTION "internal"."announce_user_delete"() OWNER TO "postgres";
+CREATE OR REPLACE TRIGGER "announce_user_delete"
+    AFTER DELETE
+    ON "auth"."users"
+    FOR EACH ROW
+EXECUTE FUNCTION "internal"."announce_user_delete"();
+SELECT "internal"."purgeolddata"();
