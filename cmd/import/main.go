0d62ac802cbf865bcbd37edf34d3a3eb5c407033
Chris Smith <chris@chameth.com>
2023-07-10 20:58:13 +0100

    import: don't create json by string concatting 😱

--- cmd/import/main.go
+++ cmd/import/main.go
@@ -29,10 +29,16 @@ func main() {
 	}
 }
 
+type authRequest struct {
+	Identity string `json:"identity"`
+	Password string `json:"password"`
+}
+
+type authResponse struct {
+	Token string `json:"token"`
+}
+
 func uploadAdventures(fileName, apiurl string, email string, password string) error {
-	data := struct {
-		Token string
-	}{}
 	file, err := os.Open(fileName)
 	if err != nil {
 		return fmt.Errorf("unable to open zip: %s", err)
@@ -40,6 +46,8 @@ func uploadAdventures(fileName, apiurl string, email string, password string) er
 	defer func() {
 		_ = file.Close()
 	}()
+
+	var response authResponse
 	//TODO: Should remove resty really, its overkill now I'm just uploading a file
 	client := resty.New()
 	client.SetRetryCount(3).
@@ -47,9 +55,13 @@ func uploadAdventures(fileName, apiurl string, email string, password string) er
 		SetRetryMaxWaitTime(10*time.Second).
 		SetBaseURL(apiurl).
 		SetHeader("Content-Type", "application/json")
+
 	resp, err := client.R().
-		SetBody(`{"identity":"` + email + `", "password":"` + password + `"}`).
-		SetResult(&data).
+		SetBody(authRequest{
+			Identity: email,
+			Password: password,
+		}).
+		SetResult(&response).
 		Post("api/admins/auth-with-password")
 	if err != nil {
 		return fmt.Errorf("unable to login")
@@ -57,7 +69,7 @@ func uploadAdventures(fileName, apiurl string, email string, password string) er
 	if resp.StatusCode() != 200 {
 		return fmt.Errorf("unable to login: Status %d", resp.StatusCode())
 	}
-	client.SetAuthToken(data.Token)
+	client.SetAuthToken(response.Token)
 	resp, err = client.R().
 		SetContentLength(true).
 		SetFileReader("adventures.zip", "adventures.zip", file).
